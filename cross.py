a0e4f0d454346c995d644b37ae17b7f21e054da7
Ivan Kadochnikov <kadivas@jinr.ru>
2021-06-23 15:35:35 +0300

    Rewrite cross energy matrix without layers

--- cross.py
+++ cross.py
@@ -2,32 +2,43 @@ from typing import Union, List
 
 import numpy as np
 from numpy import ndarray
-from scipy import sparse
-from scipy.sparse import csr_matrix, spmatrix
+from scipy.sparse import csr_matrix, spmatrix, coo_matrix
 
 
-def fork_layer_matrix(segments: ndarray) -> csr_matrix:
-    if len(segments) < 1:
-        return csr_matrix(np.zeros((0, 0)))
-    forks = (segments[:, None, 0] == segments[None, :, 0]).astype(float)
-    np.fill_diagonal(forks, 0)
-    return csr_matrix(forks)
+def segment_one_forks(seg: ndarray, i: int) -> ndarray:
+    is_fork = np.equal(seg[:, 0], seg[i, 0])
+    is_fork[i] = False
+    jj = np.arange(len(seg))[is_fork]
+    segments = np.stack(np.broadcast_arrays(i, jj), axis=1)
+    return segments
 
 
-def join_layer_matrix(segments: ndarray) -> csr_matrix:
-    if len(segments) < 1:
-        return csr_matrix(np.zeros((0, 0)))
-    joins = (segments[:, None, 1] == segments[None, :, 1]).astype(float)
-    np.fill_diagonal(joins, 0)
-    return csr_matrix(joins)
+def segment_forks(seg: ndarray) -> ndarray:
+    if len(seg) == 0:
+        return np.empty((0, 2), dtype=int)
+    return np.concatenate([segment_one_forks(seg, s) for s in range(len(seg))])
+
+
+def segment_one_joins(seg: ndarray, i: int) -> ndarray:
+    is_join = np.equal(seg[:, 1], seg[i, 1])
+    is_join[i] = False
+    jj = np.arange(len(seg))[is_join]
+    segments = np.stack(np.broadcast_arrays(i, jj), axis=1)
+    return segments
+
+
+def segment_joins(seg: ndarray) -> ndarray:
+    if len(seg) == 0:
+        return np.empty((0, 2), dtype=int)
+    return np.concatenate([segment_one_joins(seg, s) for s in range(len(seg))])
 
 
 def cross_energy_matrix(segments: List[ndarray]) -> csr_matrix:
     if len(segments) < 1:
         return csr_matrix(np.zeros((0, 0)))
-    return sparse.block_diag(
-        [fork_layer_matrix(s).tocsr() + join_layer_matrix(s).tocsr() for s in segments],
-        format="csr")
+    seg = np.concatenate(segments)
+    crosses = np.concatenate([segment_forks(seg), segment_joins(seg)])
+    return coo_matrix((np.ones(len(crosses)), crosses.transpose()), shape=(len(seg), len(seg))).tocsr()
 
 
 def cross_energy(matrix: Union[ndarray, spmatrix], activation: ndarray) -> float:
--- cross_test.py
+++ cross_test.py
@@ -2,49 +2,51 @@ import numpy as np
 from numpy.testing import assert_array_equal, assert_array_almost_equal
 from scipy.sparse import csr_matrix
 
-from cross import fork_layer_matrix, join_layer_matrix, cross_energy, cross_energy_gradient, cross_energy_matrix
+from cross import cross_energy, cross_energy_gradient, cross_energy_matrix, \
+    segment_one_forks, segment_forks, segment_one_joins, segment_joins
 
 
-def test_fork_layer_matrix():
-    m = fork_layer_matrix(np.array([]))
-    assert_array_equal(m.todense(), np.zeros((0, 0)))
-    m = fork_layer_matrix(np.array([[0, 1]]))
-    assert_array_equal(m.todense(), [[0]])
-    m = fork_layer_matrix(np.array([[0, 1], [0, 2]]))
-    assert_array_equal(m.todense(), [[0, 1], [1, 0]])
-    m = fork_layer_matrix(np.array([[0, 2], [1, 2]]))
-    assert_array_equal(m.todense(), [[0, 0], [0, 0]])
-
+def test_segment_one_forks():
+    assert_array_equal(segment_one_forks(np.array([[0, 1]]), 0), np.empty((0, 2)))
+    assert_array_equal(segment_one_forks(np.array([[0, 1], [0, 2]]), 0), [[0, 1]])
+    assert_array_equal(segment_one_forks(np.array([[0, 2], [1, 2]]), 0), np.empty((0, 2)))
     segments = np.array([[0, 2], [0, 3], [1, 2], [1, 3]])
-    m = fork_layer_matrix(segments)
-    assert_array_equal(m.todense(), [[0, 1, 0, 0],
-                                     [1, 0, 0, 0],
-                                     [0, 0, 0, 1],
-                                     [0, 0, 1, 0]])
+    assert_array_equal(segment_one_forks(segments, 2), [[2, 3]])
 
 
-def test_join_layer_matrix():
-    m = join_layer_matrix(np.array([]))
-    assert_array_equal(m.todense(), np.zeros((0, 0)))
-    m = join_layer_matrix(np.array([[0, 1]]))
-    assert_array_equal(m.todense(), [[0]])
-    m = join_layer_matrix(np.array([[0, 1], [0, 2]]))
-    assert_array_equal(m.todense(), [[0, 0], [0, 0]])
-    m = join_layer_matrix(np.array([[0, 2], [1, 2]]))
-    assert_array_equal(m.todense(), [[0, 1], [1, 0]])
-
+def test_segment_forks():
+    null_segments = np.empty((0, 2), dtype=int)
+    assert_array_equal(segment_forks(null_segments), np.empty((0, 2)))
+    assert_array_equal(segment_forks(np.array([[0, 1]])), np.empty((0, 2)))
+    assert_array_equal(segment_forks(np.array([[0, 1], [0, 2]])), [[0, 1], [1, 0]])
+    assert_array_equal(segment_forks(np.array([[0, 2], [1, 2]])), np.empty((0, 2)))
     segments = np.array([[0, 2], [0, 3], [1, 2], [1, 3]])
-    m = join_layer_matrix(segments)
-    assert_array_equal(m.todense(), [[0, 0, 1, 0],
-                                     [0, 0, 0, 1],
-                                     [1, 0, 0, 0],
-                                     [0, 1, 0, 0]])
+    assert_array_equal(segment_forks(segments), [[0, 1], [1, 0], [2, 3], [3, 2]])
+
+
+def test_segment_one_joins():
+    assert_array_equal(segment_one_joins(np.array([[0, 1]]), 0), np.empty((0, 2)))
+    assert_array_equal(segment_one_joins(np.array([[0, 1], [0, 2]]), 0), np.empty((0, 2)))
+    assert_array_equal(segment_one_joins(np.array([[0, 2], [1, 2]]), 0), [[0, 1]])
+    segments = np.array([[0, 2], [0, 3], [1, 2], [1, 3]])
+    assert_array_equal(segment_one_joins(segments, 2), [[2, 0]])
+
+
+def test_segment_joins():
+    null_segments = np.empty((0, 2), dtype=int)
+    assert_array_equal(segment_joins(null_segments), np.empty((0, 2)))
+    assert_array_equal(segment_joins(np.array([[0, 1]])), np.empty((0, 2)))
+    assert_array_equal(segment_joins(np.array([[0, 1], [0, 2]])), np.empty((0, 2)))
+    assert_array_equal(segment_joins(np.array([[0, 2], [1, 2]])), [[0, 1], [1, 0]])
+    segments = np.array([[0, 2], [0, 3], [1, 2], [1, 3]])
+    assert_array_equal(segment_joins(segments), [[0, 2], [1, 3], [2, 0], [3, 1]])
 
 
 def test_cross_energy_matrix():
+    null_segments = np.empty((0, 2), dtype=int)
     assert cross_energy_matrix([]).shape == (0, 0)
-    assert cross_energy_matrix([np.array([]), np.array([])]).shape == (0, 0)
-    segments = [np.array([]), np.array([[0, 1]]),
+    assert cross_energy_matrix([null_segments, null_segments]).shape == (0, 0)
+    segments = [null_segments, np.array([[0, 1]]),
                 np.array([[7, 11], [7, 12]]), np.array([[8, 13], [9, 13]]),
                 np.array([[5, 2], [5, 3], [6, 2], [6, 3]])]
     assert_array_equal(cross_energy_matrix(segments).A,
