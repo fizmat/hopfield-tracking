43a01e693ddf9deda20d81503c85a3add0e2fca9
Ivan Kadochnikov <kadivas@jinr.ru>
2021-06-24 15:53:18 +0300

    Generate spiral tracks in magnetic field

--- generator.py
+++ generator.py
@@ -8,11 +8,15 @@ from numpy.random import default_rng
 
 
 class SimpleEventGenerator:
-    def __init__(self, halfwidth_degrees: float = 15, n_layers: int = 8, layers_thickness=0.5, seed=None):
+    def __init__(self, halfwidth_degrees: float = 15, n_layers: int = 8, layers_thickness=0.5,
+                 field_strength=1., seed=None):
         self.halfwidth = math.radians(halfwidth_degrees)
         self.rng = default_rng(seed)
         self.n_layers = n_layers
         self.layers_thickness = layers_thickness
+        self.field_strength = field_strength
+        self.layer_i = np.arange(n_layers)
+        self.layer_x = self.layers_thickness + self.layers_thickness * self.layer_i
 
     def gen_directions_in_cone(self, n: int = 1) -> ndarray:
         x = self.rng.uniform(math.cos(self.halfwidth), 1., n)
@@ -25,14 +29,30 @@ class SimpleEventGenerator:
     def gen_momentum(self, n: int = 1) -> ndarray:
         return self.rng.gamma(shape=5., scale=1., size=n)
 
+    def run_straight_track(self, m: np.ndarray) -> ndarray:
+        track_vector = m / m[0]  # scale track vector to put x-component at 1
+        hits = track_vector[None, :] * self.layer_x[:, None]  # [layer, coordinate]
+        return hits
+
+    def run_curved_track(self, m: np.ndarray):
+        m_yz = np.linalg.norm(m[1:])
+        r = m_yz / self.field_strength
+        # to_center = np.array([[0, 1], [0, -1]]).dot(momentum[1:]) / m
+        center = np.array([[0, -1], [1, 0]]).dot(m[1:]) / self.field_strength  # r * to_center
+        # TC = 2*pi*r/v_xy  TL = h/v_z
+        arc_per_dist = self.layers_thickness * self.field_strength / m[0]
+        angles = np.arctan2(m[2], m[1]) + arc_per_dist * self.layer_x
+        hits = np.stack([self.layer_x, center[0] + r * np.sin(angles), center[1] - r * np.cos(angles)], axis=-1)
+        return hits
+
     def run_track(self, momentum: np.ndarray) -> Tuple[pd.DataFrame, ndarray]:
-        layer_i = np.arange(self.n_layers)
-        layer_x = self.layers_thickness + self.layers_thickness * layer_i
-        track_vector = momentum / momentum[0]  # scale track vector to put x-component at 1
-        hits = track_vector[None, :] * layer_x[:, None]  # [layer, coordinate]
+        if self.field_strength > 0:
+            hits = self.run_curved_track(momentum)
+        else:
+            hits = self.run_straight_track(momentum)
         hits[:, 1:] += self.rng.normal(0, 0.005, hits[:, 1:].shape)  # inaccuracy in y and z
-        data = np.concatenate([hits, layer_i[:, np.newaxis]], axis=1)
-        ii = np.arange(self.n_layers - 1)
+        data = np.concatenate([hits, self.layer_i[:, np.newaxis]], axis=1)
+        ii = self.layer_i[:-1]
         seg = np.stack([ii, ii + 1], axis=1)
         return pd.DataFrame(data, columns=['x', 'y', 'z', 'layer']), seg
 
--- generator_test.py
+++ generator_test.py
@@ -13,14 +13,53 @@ def test_gen_directions_in_cone():
                     [0, 0, 0], atol=0.01)
 
 
-def test_gen_event():
+def test_run_straight_track():
+    g = SimpleEventGenerator(field_strength=0.)
+    assert_array_equal(g.run_straight_track(np.array([1, 0, 0])),
+                       [[0.5, 0, 0], [1, 0, 0], [1.5, 0, 0], [2, 0, 0],
+                        [2.5, 0, 0], [3, 0, 0], [3.5, 0, 0], [4, 0, 0]])
+    assert_array_equal(g.run_straight_track(np.array([1, 1, -1])),
+                       [[0.5, 0.5, -0.5], [1, 1, -1], [1.5, 1.5, -1.5], [2, 2, -2],
+                        [2.5, 2.5, -2.5], [3, 3, -3], [3.5, 3.5, -3.5], [4, 4, -4]])
+
+
+def test_run_curved_track():
+    g = SimpleEventGenerator(field_strength=1.)
+    assert_array_equal(g.run_curved_track(np.array([1, 0, 0])),
+                       [[0.5, 0, 0], [1, 0, 0], [1.5, 0, 0], [2, 0, 0],
+                        [2.5, 0, 0], [3, 0, 0], [3.5, 0, 0], [4, 0, 0]])
+    r2 = np.sqrt(2) / 2
+    assert_allclose(g.run_curved_track(np.array([1 / np.pi, 0, -1])),
+                    [[0.5, 1 - r2, -r2], [1, 1, -1], [1.5, 1 + r2, -r2], [2, 2, 0],
+                     [2.5, 1 + r2, r2], [3, 1, 1], [3.5, 1 - r2, r2], [4, 0, 0]], atol=1e-14)
+
+
+def test_gen_event_nonmagnetic():
     a = np.array([[1, 0, 0], [1, 1, -1]])
-    hits, seg = SimpleEventGenerator().gen_event(a)
+    hits, seg = SimpleEventGenerator(field_strength=0.).gen_event(a)
     assert_array_equal(hits.index, range(16))
     assert list(hits.columns) == ['x', 'y', 'z', 'layer', 'track']
     assert_allclose(hits[['y', 'z']].values, [[0, 0]] * 8 +
                     [[0.5, -0.5], [1, -1], [1.5, -1.5], [2, -2],
                      [2.5, -2.5], [3, -3], [3.5, -3.5], [4, -4]], atol=0.03)
+    assert_array_equal(hits[['x', 'layer', 'track']].values,
+                       [[0.5, 0, 0], [1., 1, 0], [1.5, 2, 0], [2., 3, 0],
+                        [2.5, 4, 0], [3., 5, 0], [3.5, 6, 0], [4., 7, 0],
+                        [0.5, 0, 1], [1., 1, 1], [1.5, 2, 1], [2., 3, 1],
+                        [2.5, 4, 1], [3., 5, 1], [3.5, 6, 1], [4., 7, 1]])
+    assert_array_equal(seg, [[0, 1], [1, 2], [2, 3], [3, 4], [4, 5], [5, 6], [6, 7],
+                             [8, 9], [9, 10], [10, 11], [11, 12], [12, 13], [13, 14], [14, 15]])
+
+
+def test_gen_event_magnetic():
+    momenta = np.array([[1, 0, 0], [1 / np.pi, 0, -1]])
+    hits, seg = SimpleEventGenerator().gen_event(momenta)
+    assert_array_equal(hits.index, range(16))
+    assert list(hits.columns) == ['x', 'y', 'z', 'layer', 'track']
+    r2 = np.sqrt(2) / 2
+    assert_allclose(hits[['y', 'z']].values, [[0, 0]] * 8 +
+                    [[1 - r2, -r2], [1, -1], [1 + r2, -r2], [2, 0],
+                     [1 + r2, r2], [1, 1], [1 - r2, r2], [0, 0]], atol=0.03)
     assert_array_equal(hits[['x', 'layer', 'track']].values,
                        [[0.5, 0, 0], [1., 1, 0], [1.5, 2, 0], [2., 3, 0],
                         [2.5, 4, 0], [3., 5, 0], [3.5, 6, 0], [4., 7, 0],
