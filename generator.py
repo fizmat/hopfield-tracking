3fcda626be57940a9d2c618a258ee1712cbb6148
Ivan Kadochnikov <kadivas@jinr.ru>
2021-06-24 17:29:47 +0300

    Generate random charge and momentum

--- generator.py
+++ generator.py
@@ -26,28 +26,25 @@ class SimpleEventGenerator:
         z = r * np.sin(alpha)
         return np.stack([x, y, z], axis=1)
 
-    def gen_momentum(self, n: int = 1) -> ndarray:
-        return self.rng.gamma(shape=5., scale=1., size=n)
-
     def run_straight_track(self, m: np.ndarray) -> ndarray:
         track_vector = m / m[0]  # scale track vector to put x-component at 1
         hits = track_vector[None, :] * self.layer_x[:, None]  # [layer, coordinate]
         return hits
 
-    def run_curved_track(self, m: np.ndarray):
+    def run_curved_track(self, m: np.ndarray, q: float):
         m_yz = np.linalg.norm(m[1:])
-        r = m_yz / self.field_strength
+        r = m_yz / abs(self.field_strength * q)
         # to_center = np.array([[0, 1], [0, -1]]).dot(momentum[1:]) / m
-        center = np.array([[0, -1], [1, 0]]).dot(m[1:]) / self.field_strength  # r * to_center
+        center = np.array([[0, -1], [1, 0]]).dot(m[1:]) / (self.field_strength * q)  # r * to_center
         # TC = 2*pi*r/v_xy  TL = h/v_z
-        arc_per_dist = self.layers_thickness * self.field_strength / m[0]
-        angles = np.arctan2(m[2], m[1]) + arc_per_dist * self.layer_x
-        hits = np.stack([self.layer_x, center[0] + r * np.sin(angles), center[1] - r * np.cos(angles)], axis=-1)
+        arc_per_dist = self.layers_thickness * self.field_strength * q / m[0]
+        angles = np.arctan2(-center[1], -center[0]) + arc_per_dist * self.layer_x
+        hits = np.stack([self.layer_x, center[0] + r * np.cos(angles), center[1] + r * np.sin(angles)], axis=-1)
         return hits
 
-    def run_track(self, momentum: np.ndarray) -> Tuple[pd.DataFrame, ndarray]:
-        if self.field_strength > 0:
-            hits = self.run_curved_track(momentum)
+    def run_track(self, momentum: np.ndarray, charge: float) -> Tuple[pd.DataFrame, ndarray]:
+        if self.field_strength != 0 and charge != 0:
+            hits = self.run_curved_track(momentum, charge)
         else:
             hits = self.run_straight_track(momentum)
         hits[:, 1:] += self.rng.normal(0, 0.005, hits[:, 1:].shape)  # inaccuracy in y and z
@@ -56,20 +53,26 @@ class SimpleEventGenerator:
         seg = np.stack([ii, ii + 1], axis=1)
         return pd.DataFrame(data, columns=['x', 'y', 'z', 'layer']), seg
 
-    def gen_event(self, momenta: np.ndarray) -> Tuple[pd.DataFrame, ndarray]:
+    def gen_event(self, momenta: np.ndarray, charges: np.ndarray) -> Tuple[pd.DataFrame, ndarray]:
         hits = []
         seg = []
         n = 0
-        for i, m in enumerate(momenta):
-            h, s = self.run_track(m)
+        for i, (m, q) in enumerate(zip(momenta, charges)):
+            h, s = self.run_track(m, q)
             h.index += n
             s += n
             h['track'] = i
+            h['charge'] = q
             hits.append(h)
             seg.append(s)
             n += len(h)
         return pd.concat(hits), np.concatenate(seg)
 
-    def gen_many_events(self, n: int = 1000, event_size: int = 10) -> Generator[pd.DataFrame, None, None]:
+    def gen_many_events(self, n: int = 1000, event_size: int = 10, momentum_scale=.2) \
+            -> Generator[pd.DataFrame, None, None]:
         for _ in range(n):
-            yield self.gen_event(self.gen_directions_in_cone(event_size))
+            yield self.gen_event(
+                self.rng.gamma(shape=5., scale=momentum_scale, size=event_size)[:, np.newaxis]
+                * self.gen_directions_in_cone(event_size),
+                charges=self.rng.choice([1, -1], event_size)
+            )
--- generator_test.py
+++ generator_test.py
@@ -25,46 +25,77 @@ def test_run_straight_track():
 
 def test_run_curved_track():
     g = SimpleEventGenerator(field_strength=1.)
-    assert_array_equal(g.run_curved_track(np.array([1, 0, 0])),
+    assert_array_equal(g.run_curved_track(np.array([1, 0, 0]), 123.),
                        [[0.5, 0, 0], [1, 0, 0], [1.5, 0, 0], [2, 0, 0],
                         [2.5, 0, 0], [3, 0, 0], [3.5, 0, 0], [4, 0, 0]])
     r2 = np.sqrt(2) / 2
-    assert_allclose(g.run_curved_track(np.array([1 / np.pi, 0, -1])),
+    assert_allclose(g.run_curved_track(np.array([1 / np.pi, 0, -1]), 1.0),
                     [[0.5, 1 - r2, -r2], [1, 1, -1], [1.5, 1 + r2, -r2], [2, 2, 0],
                      [2.5, 1 + r2, r2], [3, 1, 1], [3.5, 1 - r2, r2], [4, 0, 0]], atol=1e-14)
+    assert_allclose(g.run_curved_track(np.array([1 / np.pi, 0, -1]), -1.0),
+                    [[0.5, - 1 + r2, -r2], [1, -1, -1], [1.5, -1 - r2, -r2], [2, -2, 0],
+                     [2.5, -1 - r2, r2], [3, -1, 1], [3.5, -1 + r2, r2], [4, 0, 0]], atol=1e-14)
 
 
 def test_gen_event_nonmagnetic():
     a = np.array([[1, 0, 0], [1, 1, -1]])
-    hits, seg = SimpleEventGenerator(field_strength=0.).gen_event(a)
+    hits, seg = SimpleEventGenerator(field_strength=0.).gen_event(a, np.ones(2))
     assert_array_equal(hits.index, range(16))
-    assert list(hits.columns) == ['x', 'y', 'z', 'layer', 'track']
+    assert list(hits.columns) == ['x', 'y', 'z', 'layer', 'track', 'charge']
     assert_allclose(hits[['y', 'z']].values, [[0, 0]] * 8 +
                     [[0.5, -0.5], [1, -1], [1.5, -1.5], [2, -2],
                      [2.5, -2.5], [3, -3], [3.5, -3.5], [4, -4]], atol=0.03)
-    assert_array_equal(hits[['x', 'layer', 'track']].values,
-                       [[0.5, 0, 0], [1., 1, 0], [1.5, 2, 0], [2., 3, 0],
-                        [2.5, 4, 0], [3., 5, 0], [3.5, 6, 0], [4., 7, 0],
-                        [0.5, 0, 1], [1., 1, 1], [1.5, 2, 1], [2., 3, 1],
-                        [2.5, 4, 1], [3., 5, 1], [3.5, 6, 1], [4., 7, 1]])
+    assert_array_equal(hits[['x', 'layer', 'track', 'charge']].values,
+                       [[0.5, 0, 0, 1], [1., 1, 0, 1], [1.5, 2, 0, 1], [2., 3, 0, 1],
+                        [2.5, 4, 0, 1], [3., 5, 0, 1], [3.5, 6, 0, 1], [4., 7, 0, 1],
+                        [0.5, 0, 1, 1], [1., 1, 1, 1], [1.5, 2, 1, 1], [2., 3, 1, 1],
+                        [2.5, 4, 1, 1], [3., 5, 1, 1], [3.5, 6, 1, 1], [4., 7, 1, 1]])
+    assert_array_equal(seg, [[0, 1], [1, 2], [2, 3], [3, 4], [4, 5], [5, 6], [6, 7],
+                             [8, 9], [9, 10], [10, 11], [11, 12], [12, 13], [13, 14], [14, 15]])
+
+    hits, seg = SimpleEventGenerator(field_strength=11.).gen_event(a, np.zeros(2))
+    assert_array_equal(hits.index, range(16))
+    assert list(hits.columns) == ['x', 'y', 'z', 'layer', 'track', 'charge']
+    assert_allclose(hits[['y', 'z']].values, [[0, 0]] * 8 +
+                    [[0.5, -0.5], [1, -1], [1.5, -1.5], [2, -2],
+                     [2.5, -2.5], [3, -3], [3.5, -3.5], [4, -4]], atol=0.03)
+    assert_array_equal(hits[['x', 'layer', 'track', 'charge']].values,
+                       [[0.5, 0, 0, 0], [1., 1, 0, 0], [1.5, 2, 0, 0], [2., 3, 0, 0],
+                        [2.5, 4, 0, 0], [3., 5, 0, 0], [3.5, 6, 0, 0], [4., 7, 0, 0],
+                        [0.5, 0, 1, 0], [1., 1, 1, 0], [1.5, 2, 1, 0], [2., 3, 1, 0],
+                        [2.5, 4, 1, 0], [3., 5, 1, 0], [3.5, 6, 1, 0], [4., 7, 1, 0]])
     assert_array_equal(seg, [[0, 1], [1, 2], [2, 3], [3, 4], [4, 5], [5, 6], [6, 7],
                              [8, 9], [9, 10], [10, 11], [11, 12], [12, 13], [13, 14], [14, 15]])
 
 
 def test_gen_event_magnetic():
     momenta = np.array([[1, 0, 0], [1 / np.pi, 0, -1]])
-    hits, seg = SimpleEventGenerator().gen_event(momenta)
+    hits, seg = SimpleEventGenerator().gen_event(momenta, np.ones(2))
     assert_array_equal(hits.index, range(16))
-    assert list(hits.columns) == ['x', 'y', 'z', 'layer', 'track']
+    assert list(hits.columns) == ['x', 'y', 'z', 'layer', 'track', 'charge']
     r2 = np.sqrt(2) / 2
     assert_allclose(hits[['y', 'z']].values, [[0, 0]] * 8 +
                     [[1 - r2, -r2], [1, -1], [1 + r2, -r2], [2, 0],
                      [1 + r2, r2], [1, 1], [1 - r2, r2], [0, 0]], atol=0.03)
-    assert_array_equal(hits[['x', 'layer', 'track']].values,
-                       [[0.5, 0, 0], [1., 1, 0], [1.5, 2, 0], [2., 3, 0],
-                        [2.5, 4, 0], [3., 5, 0], [3.5, 6, 0], [4., 7, 0],
-                        [0.5, 0, 1], [1., 1, 1], [1.5, 2, 1], [2., 3, 1],
-                        [2.5, 4, 1], [3., 5, 1], [3.5, 6, 1], [4., 7, 1]])
+    assert_array_equal(hits[['x', 'layer', 'track', 'charge']].values,
+                       [[0.5, 0, 0, 1], [1., 1, 0, 1], [1.5, 2, 0, 1], [2., 3, 0, 1],
+                        [2.5, 4, 0, 1], [3., 5, 0, 1], [3.5, 6, 0, 1], [4., 7, 0, 1],
+                        [0.5, 0, 1, 1], [1., 1, 1, 1], [1.5, 2, 1, 1], [2., 3, 1, 1],
+                        [2.5, 4, 1, 1], [3., 5, 1, 1], [3.5, 6, 1, 1], [4., 7, 1, 1]])
+    assert_array_equal(seg, [[0, 1], [1, 2], [2, 3], [3, 4], [4, 5], [5, 6], [6, 7],
+                             [8, 9], [9, 10], [10, 11], [11, 12], [12, 13], [13, 14], [14, 15]])
+
+    hits, seg = SimpleEventGenerator(field_strength=-1).gen_event(momenta, -np.ones(2)) # negative field and negative charge!
+    assert_array_equal(hits.index, range(16))
+    assert list(hits.columns) == ['x', 'y', 'z', 'layer', 'track', 'charge']
+    assert_allclose(hits[['y', 'z']].values, [[0, 0]] * 8 +
+                    [[1 - r2, -r2], [1, -1], [1 + r2, -r2], [2, 0],
+                     [1 + r2, r2], [1, 1], [1 - r2, r2], [0, 0]], atol=0.03)
+    assert_array_equal(hits[['x', 'layer', 'track', 'charge']].values,
+                       [[0.5, 0, 0, -1], [1., 1, 0, -1], [1.5, 2, 0, -1], [2., 3, 0, -1],
+                        [2.5, 4, 0, -1], [3., 5, 0, -1], [3.5, 6, 0, -1], [4., 7, 0, -1],
+                        [0.5, 0, 1, -1], [1., 1, 1, -1], [1.5, 2, 1, -1], [2., 3, 1, -1],
+                        [2.5, 4, 1, -1], [3., 5, 1, -1], [3.5, 6, 1, -1], [4., 7, 1, -1]])
     assert_array_equal(seg, [[0, 1], [1, 2], [2, 3], [3, 4], [4, 5], [5, 6], [6, 7],
                              [8, 9], [9, 10], [10, 11], [11, 12], [12, 13], [13, 14], [14, 15]])
 
@@ -74,5 +105,5 @@ def test_gen_many_events():
     assert len(data) == 5
     for hits, seg in data:
         assert len(hits.index) == 8 * 7
-        assert len(hits.columns) == 5
+        assert len(hits.columns) == 6
         assert seg.shape == (7 * 7, 2)
