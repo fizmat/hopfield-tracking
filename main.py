7a5cce7c2693bd927db2a7cc26ab3a8a71ba214a
Ivan Kadochnikov <kadivas@jinr.ru>
2021-07-07 12:37:42 +0300

    Use parameter gamma to compensate curvature energy for detector scale

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from statistics import mean
+
 import numpy as np
 import pandas as pd
 from matplotlib import pyplot as plt
@@ -19,56 +21,61 @@ pos = hits[['x', 'y', 'z']].values
 
 seg = gen_segments_all(hits)
 
-act = np.full(len(seg), 0.5)
-
-perfect_act = np.zeros_like(act)
+perfect_act = np.zeros(len(seg))
 
 track_segment_set = set(tuple(s) for s in track_segments)
 is_in_track = np.array([tuple(s) in track_segment_set for s in seg])
 perfect_act[is_in_track] = 1
 
-ALPHA = 5.  # наказание форков
-BETA = 0.  # наказание за количество активных
-BIAS = 0.2  # activation bias, can be used instead of beta
-POWER = 5  # степень косинуса в энергии за кривизну
-COS_MIN = 0.9  # минимальный косинус за который есть награда
+ALPHA = 0.6  # forks and joins
 
-DROP_SELF_ACTIVATION_WEIGHTS = True
+BETA = 0  # total activation
+DROP_SELF_ACTIVATION_WEIGHTS = True  # in total activation matrix
+BIAS = 0.2  # activation bias, instead of total activation matrix
 
-THRESHOLD = 0.3  # граница отсечения активации треков от не-треков
+COSINE_POWER = 5
+COSINE_MIN = 0.7
+DISTANCE_POWER = 1
+GAMMA = mean((xa - xb) * (xb - xc)
+             for xa, la in hits[['x', 'layer']].values
+             for xb in hits[hits.layer == la + 1].x
+             for xc in hits[hits.layer == la + 2].x) ** DISTANCE_POWER
 
-TMAX = 40
-TMIN = 0.1
-ANNEAL_ITERATIONS = 200
+THRESHOLD = 0.5  # activation threshold for segment classification
+
+TMAX = 20
+TMIN = 0.5
+ANNEAL_ITERATIONS = 40
 STABLE_ITERATIONS = 200
 
 DROPOUT = 0.5
-LEARNING_RATE = 0.2
-EPS = 1e-4
+LEARNING_RATE = 0.6
+MIN_ACTIVATION_CHANGE_TO_CONTINUE = 1e-9
 
 temp_curve = annealing_curve(TMIN, TMAX, ANNEAL_ITERATIONS, STABLE_ITERATIONS)
 plt.plot(temp_curve)
 
+act = np.full(len(seg), THRESHOLD * 0.999)
 acts = []
 
 a, b, c = total_activation_matrix(pos, seg, DROP_SELF_ACTIVATION_WEIGHTS)
 crossing_matrix = cross_energy_matrix(seg)
-curvature_matrix = curvature_energy_matrix(pos, seg, POWER, COS_MIN)
+curvature_matrix = curvature_energy_matrix(pos, seg, COSINE_POWER, COSINE_MIN, DISTANCE_POWER)
 for i, t in enumerate(temp_curve):
     acts.append(act)
-    grad = curvature_energy_gradient(curvature_matrix, act) + \
+    grad = GAMMA * curvature_energy_gradient(curvature_matrix, act) + \
            ALPHA * cross_energy_gradient(crossing_matrix, act) + \
            BETA * total_activation_energy_gradient(a, b, act)
     a_prev = act
     act = update_layer_grad(a_prev, grad, t, DROPOUT, LEARNING_RATE, BIAS)
-    if np.abs(act - a_prev).sum() < EPS and i > ANNEAL_ITERATIONS:
+    if np.abs(act - a_prev).sum() < MIN_ACTIVATION_CHANGE_TO_CONTINUE and i > ANNEAL_ITERATIONS:
         break
 
 energy_history = []
 for act in acts:
     en = BETA * total_activation_energy(a, b, c, act)
     ef = ALPHA * cross_energy(crossing_matrix, act)
-    ec = - curvature_energy(curvature_matrix, act)
+    ec = - GAMMA * curvature_energy(curvature_matrix, act)
     energy_history.append([ec, en, ef])
 energy_history = pd.DataFrame(energy_history, columns=['E_curve', 'E_number', 'E_fork'])
 
