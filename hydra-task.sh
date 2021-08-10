8f1f90f511ad83dc30d71947ea6e14f21e99ee13
Ivan Kadochnikov <kadivas@jinr.ru>
2021-08-10 14:20:30 +0300

    Pass slurm job arguments to run script

--- hydra-task.sh
+++ hydra-task.sh
@@ -1,10 +1,22 @@
-#!/bin/sh
+#!/bin/bash
+
+n_tracks=${n_tracks:-10}
+max_budget=${max_budget:-100}
+n_iterations=${n_itreations:-10}
+n_workers=${n_workers:-1}
+
 if [ $SLURM_PROCID -eq 0 ]
 then
     date
-    /tmp/ikadochn/env-hop/bin/python optimize.py --n_tracks 10 --max_budget 1000 --n_iterations=500 --run_id "$SLURM_JOB_ID" --nic_name eth0 --shared_directory ./workdir --n_workers 80
+    "/tmp/$USER/env-hop/bin/python" optimize.py --n_tracks $n_tracks \
+    --max_budget $max_budget --n_iterations $n_iterations \
+    --run_id "$SLURM_JOB_ID" --nic_name enp4s0f0 \
+    --shared_directory ./workdir --n_workers $n_workers
     date
 else
-    /tmp/ikadochn/env-hop/bin/python optimize.py --n_tracks 10 --max_budget 1000 --n_iterations=500 --run_id "$SLURM_JOB_ID" --nic_name eth0 --shared_directory ./workdir --worker
+    "/tmp/$USER/env-hop/bin/python" optimize.py --n_tracks $n_tracks \
+    --max_budget $max_budget --n_iterations $n_iterations \
+    --run_id "$SLURM_JOB_ID" --nic_name enp4s0f0 \
+    --shared_directory ./workdir --worker
 fi
 
