d5c181a27224fc19f85433ae5d381f6680762f5e
Ivan Kadochnikov <kadivas@jinr.ru>
2021-08-10 16:05:08 +0300

    Add sbatch notebook, fix script errors

--- hydra-task.sh
+++ hydra-task.sh
@@ -8,13 +8,13 @@ n_workers=${n_workers:-1}
 if [ $SLURM_PROCID -eq 0 ]
 then
     date
-    "/tmp/$USER/env-hop/bin/python" optimize.py --n_tracks $n_tracks \
+    "/tmp/$USER/miniconda3/envs/hopfield-tracking/bin/python" optimize.py --n_tracks $n_tracks \
     --max_budget $max_budget --n_iterations $n_iterations \
     --run_id "$SLURM_JOB_ID" --nic_name enp4s0f0 \
     --shared_directory ./workdir --n_workers $n_workers
     date
 else
-    "/tmp/$USER/env-hop/bin/python" optimize.py --n_tracks $n_tracks \
+    "/tmp/$USER/miniconda3/envs/hopfield-tracking/bin/python" optimize.py --n_tracks $n_tracks \
     --max_budget $max_budget --n_iterations $n_iterations \
     --run_id "$SLURM_JOB_ID" --nic_name enp4s0f0 \
     --shared_directory ./workdir --worker
