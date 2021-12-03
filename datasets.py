689c95b4e6dd708f613de94b4a50e55ec4abe624
Alexandr Kovalenko <kovalen@space17.hydra.local>
2021-12-03 19:39:02 +0300

    library datasets with Input_hits_TrackML Input_blacklist_TrackML

--- datasets.py
+++ datasets.py
@@ -1,31 +1,27 @@
 import numpy as np
 
-def inputTrackML(fileTrackML):
+def input_hits_TrackML(event_file):
     import pandas as pd
       
     hits_truth=[]
-    
-    file_hits = fileTrackML+'-hits.csv'
-    file_particles = fileTrackML+'-particles.csv'
-    file_truth = fileTrackML+'-truth.csv'
-    file_cells = fileTrackML+'-cells.csv'
-    file_blacklist_hits = fileTrackML+'-blacklist_hits.csv'
-    file_blacklist_particles = fileTrackML+'-blacklist_particles.csv'
-           
+    file_hits = 'datasets/trackml/train_100_events/'+event_file+'-hits.csv'
+    file_truth = 'datasets/trackml/train_100_events/'+event_file+'-truth.csv'           
     hits = pd.read_csv(file_hits)
-    particle = pd.read_csv(file_particles)
     truth = pd.read_csv(file_truth)
-    cels = pd.read_csv(file_cells)
+    hits_truth = hits.merge(truth, left_on='hit_id', right_on='hit_id', suffixes=('_left', '_right'))
+    return hits_truth
+
+def input_blacklist_TrackML(event_file):
+    import pandas as pd
+    
+    file_blacklist_hits = 'datasets/trackml/blacklist/'+event_file+'-blacklist_hits.csv'
+    file_blacklist_particles = 'datasets/trackml/blacklist/'+event_file+'-blacklist_particles.csv'
     blacklist_hits = pd.read_csv(file_blacklist_hits)
     blacklist_particle = pd.read_csv(file_blacklist_particles)
-
-    hits_truth = hits.merge(truth, left_on='hit_id', right_on='hit_id', suffixes=('_left', '_right'))
-    
-    #particle_truth = particle.merge(truth, left_on=’particle_id’,right_on=’particle_id’, suffixes=(‘_left’, ‘_right’))
-    return hits_truth,blacklist_hits
+    return blacklist_hits
 
 
-def transformTrackML(hits_truth, blacklist_hits):
+def transform_TrackML(hits_truth, blacklist_hits):
     hits_trackML=hits_truth[np.logical_not(hits_truth.hit_id.isin(blacklist_hits.hit_id))]
 
     hits=hits_trackML.rename(columns={'layer_id': 'layer', 'particle_id': 'track'})
@@ -36,14 +32,14 @@ def transformTrackML(hits_truth, blacklist_hits):
     return hits 
 
 
-def inputBMaN():
+def input_BMaN():
     import pandas as pd
     simdata = pd.read_csv('simdata_ArPb_3.2AGeV_mb_1.zip', sep='\t',
                           names=['event_id', 'x', 'y', 'z', 'detector_id', 'station_id', 'track_id', 'px', 'py', 'pz', 'vx', 'vy', 'vz'])
     return(simdata)
 
 
-def transformBMaN(simdata):
+def transform_BMaN(simdata):
     cc = simdata.groupby('event_id').x.count()
     cc[cc==cc[cc<=800].max()]
     simdata['layer'] = simdata.detector_id * 3 + simdata.station_id
