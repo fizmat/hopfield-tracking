f73185ffbf5d397c50a4e3543546b5f99923bb52
Ivan Kadochnikov <kadivas@jinr.ru>
2021-12-20 16:56:41 +0300

    Rename dataset functions to lowercase

--- datasets.py
+++ datasets.py
@@ -13,7 +13,7 @@ def get_hits_simple(n_events=100, event_size=10):
     return pd.concat(hits_list, ignore_index=True)
 
 
-def _read_truth_TrackML(event_file):
+def _read_truth_trackml(event_file):
     hits_truth = []
     file_hits = 'datasets/trackml/train_100_events/' + event_file + '-hits.csv'
     file_truth = 'datasets/trackml/train_100_events/' + event_file + '-truth.csv'
@@ -23,7 +23,7 @@ def _read_truth_TrackML(event_file):
     return hits_truth
 
 
-def _read_blacklist_TrackML(event_file):
+def _read_blacklist_trackml(event_file):
     file_blacklist_hits = 'datasets/trackml/blacklist/' + event_file + '-blacklist_hits.csv'
     # file_blacklist_particles = 'datasets/trackml/blacklist/'+event_file+'-blacklist_particles.csv'
     blacklist_hits = pd.read_csv(file_blacklist_hits)
@@ -31,7 +31,7 @@ def _read_blacklist_TrackML(event_file):
     return blacklist_hits
 
 
-def _transform_TrackML(hits_truth, blacklist_hits):
+def _transform_trackml(hits_truth, blacklist_hits):
     hits_trackML = hits_truth[np.logical_not(hits_truth.hit_id.isin(blacklist_hits.hit_id))]
 
     hits = hits_trackML.rename(columns={'layer_id': 'layer', 'particle_id': 'track'})
@@ -41,7 +41,7 @@ def _transform_TrackML(hits_truth, blacklist_hits):
     return hits
 
 
-def get_hits_TrackML():
+def get_hits_trackml():
     events = []
     event_prefix = 'event00000'
 
@@ -49,26 +49,26 @@ def get_hits_TrackML():
         strnum_ev = f'{num_ev:04}'
 
         event_file = event_prefix + strnum_ev
-        hits_truth = _read_truth_TrackML(event_file)
-        blacklist = _read_blacklist_TrackML(event_file)
-        hits = _transform_TrackML(hits_truth, blacklist)
+        hits_truth = _read_truth_trackml(event_file)
+        blacklist = _read_blacklist_trackml(event_file)
+        hits = _transform_trackml(hits_truth, blacklist)
         hits['event_id'] = num_ev
         events.append(hits)
     return pd.concat(events, ignore_index=True)
 
 
-def get_hits_TrackML_by_volume():
-    hits = get_hits_TrackML()
+def get_hits_trackml_by_volume():
+    hits = get_hits_trackml()
     hits.event_id = hits.event_id.astype(str) + '-' + hits.volume_id.astype(str)
     return hits
 
 
-def get_hits_TrackML_by_module():
-    hits = get_hits_TrackML()
+def get_hits_trackml_by_module():
+    hits = get_hits_trackml()
     hits.event_id = hits.event_id.astype(str) + '-' + hits.volume_id.astype(str) + '-' + hits.module_id.astype(str)
     return hits
 
-def _read_BMaN():
+def _read_bman():
     import pandas as pd
     simdata = pd.read_csv('datasets/bman/simdata_ArPb_3.2AGeV_mb_1.zip', sep='\t',
                           names=['event_id', 'x', 'y', 'z', 'detector_id', 'station_id', 'track_id', 'px', 'py', 'pz',
@@ -76,7 +76,7 @@ def _read_BMaN():
     return (simdata)
 
 
-def _transform_BMaN(simdata, max_hits=400):
+def _transform_bman(simdata, max_hits=400):
     hit_count = simdata.groupby('event_id').size()
     small_events = set(hit_count[hit_count <= max_hits].index)
     simdata = simdata[simdata.event_id.isin(small_events)].copy()
@@ -84,5 +84,5 @@ def _transform_BMaN(simdata, max_hits=400):
     return simdata.rename(columns={'track_id': 'track'})[['x', 'y', 'z', 'layer', 'track', 'event_id']]
 
 
-def get_hits_BMaN(max_hits=400):
-    return _transform_BMaN(_read_BMaN(), max_hits)
+def get_hits_bman(max_hits=400):
+    return _transform_bman(_read_bman(), max_hits)
--- optimize.py
+++ optimize.py
@@ -18,7 +18,7 @@ from sklearn.metrics import f1_score
 
 from cross import cross_energy_matrix
 from curvature import curvature_energy_matrix, segment_adjacent_pairs
-from datasets import get_hits_TrackML_by_module, get_hits_BMaN, get_hits_simple
+from datasets import get_hits_trackml_by_module, get_hits_bman, get_hits_simple
 from reconstruct import annealing_curve, update_layer_grad, energy_gradient
 from metrics.tracks import build_segmented_tracks, found_tracks, found_crosses
 from segment import gen_segments_all
@@ -43,11 +43,11 @@ class MyWorker(Worker):
         self.max_hits = max_hits
         self.total_steps = total_steps
         if dataset.lower() == 'bman':
-            hits = get_hits_BMaN(max_hits)
+            hits = get_hits_bman(max_hits)
         elif dataset.lower() == 'simple':
             hits = get_hits_simple()
         elif dataset.lower() == 'trackml':
-            hits = get_hits_TrackML_by_module()
+            hits = get_hits_trackml_by_module()
         else:
             raise ValueError(f'Unknown dataset: {dataset}')
         events = hits.event_id.unique()
