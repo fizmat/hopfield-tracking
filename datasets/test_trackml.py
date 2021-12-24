82e55392d69880de1a71270d4e70ea00583ba092
Ivan Kadochnikov <kadivas@jinr.ru>
2021-12-24 18:50:04 +0300

    Read trackml dataset from zip archive directly

--- datasets/test_trackml.py
+++ datasets/test_trackml.py
@@ -3,7 +3,7 @@ import pytest
 from numpy.testing import assert_array_equal
 from pandas._testing import assert_frame_equal
 
-from datasets.trackml import _read_truth, _read_blacklist, _transform, get_hits_trackml, get_hits_trackml_by_module, \
+from datasets.trackml import _transform, get_hits_trackml, get_hits_trackml_by_module, \
     get_hits_trackml_by_volume, get_hits_trackml_one_event, get_hits_trackml_one_event_by_volume, \
     get_hits_trackml_one_event_by_module
 
@@ -26,34 +26,6 @@ _test_event = pd.DataFrame({
 })
 
 _test_blacklist = pd.DataFrame({'hit_id': [1, 5]})
-
-
-def test__read_truth(tmp_path):
-    d = tmp_path / 'test_datasets_trackml'
-    d.mkdir()
-    hits = ('hit_id,x,y,z,volume_id,layer_id,module_id\n' +
-            '1,1.0,2.0,3.0,7,2,1\n' +
-            '2,2.0,3.0,4.0,7,2,1\n' +
-            '3,3.0,4.0,5.0,7,2,1\n' +
-            '4,4.0,5.0,6.0,7,2,1\n' +
-            '5,5.0,6.0,7.0,7,2,1\n')
-    truth = ('hit_id,particle_id,tx,ty,tz,tpx,tpy,tpz,weight\n' +
-             '1,0,0.1,0.2,0.3,11,22,33,     0\n' +
-             '2,123456789012345678,0.2,0.3,0.4,1.1,2.2,3.3,1e-06\n' +
-             '3,0,0.3,0.4,0.5,22,33,44,     0\n' +
-             '4,123456789012345678,0.4,0.5,0.6,1.2,2.3,3.4,2e-06\n' +
-             '5,987654321098765432,0.5,0.6,0.7,1.3,2.4,3.5,3e-06\n')
-    (d / 'test-hits.csv').write_text(hits)
-    (d / 'test-truth.csv').write_text(truth)
-    events = _read_truth('test', str(d))
-    assert_frame_equal(events, _test_event)
-
-
-def test__read_blacklist(tmp_path):
-    d = tmp_path / 'test_datasets_trackml'
-    d.mkdir()
-    (d / 'test-blacklist_hits.csv').write_text('hit_id\n1\n5\n')
-    assert_frame_equal(_read_blacklist('test', str(d)), _test_blacklist)
 
 
 def test__transform():
--- datasets/trackml.py
+++ datasets/trackml.py
@@ -1,53 +1,46 @@
 from pathlib import Path
+from zipfile import ZipFile
 
 import numpy as np
 import pandas as pd
 
 
-def _read_truth(event_file: str, prefix: str = None) -> pd.DataFrame:
-    if prefix is None:
-        prefix = Path(__file__) / '../../data/trackml/train_100_events'
-    else:
-        prefix = Path(prefix)
-    hits = pd.read_csv(prefix / (event_file + '-hits.csv'))
-    truth = pd.read_csv(prefix / (event_file + '-truth.csv'))
-    return hits.merge(truth, on='hit_id')
-
-
-def _read_blacklist(event_file: str, prefix: str = None) -> pd.DataFrame:
-    if prefix is None:
-        prefix = Path(__file__) / '../../data/trackml/blacklist'
-    else:
-        prefix = Path(prefix)
-    return pd.read_csv(prefix / (event_file + '-blacklist_hits.csv'))
-
-
-def _transform(hits_truth, blacklist_hits):
-    hits_trackML = hits_truth[np.logical_not(hits_truth.hit_id.isin(blacklist_hits.hit_id))]
-    hits = hits_trackML.rename(columns={'layer_id': 'layer', 'particle_id': 'track'})
+def _transform(hits, blacklist_hits):
+    hits = hits[np.logical_not(hits.hit_id.isin(blacklist_hits.hit_id))]
+    hits = hits.rename(columns={'layer_id': 'layer', 'particle_id': 'track'})
     hits.track = hits.track.where(hits.track != 0, other=-1)
     hits.reset_index(drop=True, inplace=True)
     hits['layer'] = hits.layer // 2
     return hits
 
 
-def get_hits_trackml():
+def get_hits_trackml(zip: Path = Path(__file__) / '../../data/trackml/train_sample.zip',
+                     blacklist_zip: Path = Path(__file__) / '../../data/trackml/blacklist_training.zip') \
+        -> pd.DataFrame:
     events = []
-    event_prefix = 'event00000'
-
-    for num_ev in range(1000, 1100):
-        event_file = event_prefix + f'{num_ev:04}'
-        hits_truth = _read_truth(event_file)
-        blacklist = _read_blacklist(event_file)
-        hits = _transform(hits_truth, blacklist)
-        hits['event_id'] = num_ev
-        events.append(hits)
+    with ZipFile(zip) as z:
+        with ZipFile(blacklist_zip) as bz:
+            for event_number in range(1000, 1100):
+                with z.open(f'train_100_events/event{event_number:09}-hits.csv') as f:
+                    hits = pd.read_csv(f)
+                with z.open(f'train_100_events/event{event_number:09}-truth.csv') as f:
+                    truth = pd.read_csv(f)
+                hits = hits.merge(truth, on='hit_id')
+                with bz.open(f'event{event_number:09}-blacklist_hits.csv') as f:
+                    blacklist_hits = pd.read_csv(f)
+                hits = _transform(hits, blacklist_hits)
+                hits['event_id'] = event_number
+                events.append(hits)
     return pd.concat(events, ignore_index=True)
 
 
-def get_hits_trackml_one_event():
-    event_file = 'event000001000'
-    hits = _transform(_read_truth(event_file), _read_blacklist(event_file))
+def get_hits_trackml_one_event(path: Path = Path(__file__) / '../../data/trackml'):
+    event_number = 1000
+    hits = pd.read_csv(path / f'event{event_number:09}-hits.csv')
+    truth = pd.read_csv(path / f'event{event_number:09}-truth.csv')
+    hits = hits.merge(truth, on='hit_id')
+    blacklist_hits = pd.read_csv(path / f'event{event_number:09}-blacklist_hits.csv')
+    hits = _transform(hits, blacklist_hits)
     hits['event_id'] = 1000
     return hits
 
