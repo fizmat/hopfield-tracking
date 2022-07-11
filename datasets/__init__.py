f9847c95931ed993e9cd533df0bfaac163e1aef8
Ivan Kadochnikov <kadivas@jinr.ru>
2022-07-11 14:46:30 +0300

    Remove trackml slicing by module, not useful

--- datasets/__init__.py
+++ datasets/__init__.py
@@ -1,4 +1,4 @@
 from datasets.bman import get_hits_bman, get_hits_bman_one_event
 from datasets.simple import get_hits_simple, get_hits_simple_one_event
-from datasets.trackml import get_hits_trackml, get_hits_trackml_by_volume, get_hits_trackml_by_module, \
-    get_hits_trackml_one_event, get_hits_trackml_one_event_by_volume, get_hits_trackml_one_event_by_module
+from datasets.trackml import get_hits_trackml, get_hits_trackml_by_volume,\
+    get_hits_trackml_one_event, get_hits_trackml_one_event_by_volume
