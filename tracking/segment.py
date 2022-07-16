5611082e83552f3bece26dac0445f34645b01c61
Ivan Kadochnikov <kadivas@jinr.ru>
2022-07-16 09:45:04 +0300

    Improve performance:
    - filter by layer before radius (faster this way)
    - use numpy over pandas when possible

--- tracking/segment.py
+++ tracking/segment.py
@@ -1,7 +1,8 @@
-from typing import Iterable, List, Tuple
+from typing import List, Tuple
 
 import numpy as np
 import pandas as pd
+from line_profiler_pycharm import profile
 from numpy import ndarray
 from numpy.typing import ArrayLike
 from sklearn.neighbors import NearestNeighbors
@@ -41,31 +42,44 @@ def gen_seg_track_layered(event: pd.DataFrame) -> np.ndarray:
     return np.array(track_segments)
 
 
+@profile
 def seg_drop_same_layer(seg: np.ndarray, event: pd.DataFrame) -> np.ndarray:
-    return seg[event.loc[seg[:, 0], 'layer'] != event.loc[seg[:, 0]], 'layer']
+    a = event.loc[seg[:, 0], 'layer'].to_numpy()
+    b = event.loc[seg[:, 1], 'layer'].to_numpy()
+    comp = a != b
+    return seg[comp]
 
 
-def nbr_drop_same_layer(nbr, current_hits, event):
+@profile
+def nbr_drop_same_layer(nbr: np.ndarray, dist: np.ndarray, current_hits: pd.DataFrame,
+                        event: pd.DataFrame) -> Tuple[np.ndarray, np.ndarray]:
     nbrn = np.empty_like(nbr, dtype='object')
+    distn = np.empty_like(dist, dtype='object')
     for i, neighbor_array in enumerate(nbr):
-        comparison: pd.Series = current_hits.layer.iloc[i] != event.layer.iloc[neighbor_array]
-        nbrn[i] = neighbor_array[comparison.to_numpy()]
-    return nbrn
+        hit_layer = current_hits.layer.iloc[i]
+        nbr_layers = event.layer.to_numpy()[neighbor_array]
+        comparison = hit_layer != nbr_layers
+        nbrn[i] = neighbor_array[comparison]
+        distn[i] = dist[i][comparison]
+    return distn, nbrn
 
 
+@profile
 def nbr_stat_block(current_hits: pd.DataFrame, neighbors_model: NearestNeighbors,
                    distances: np.ndarray, event: pd.DataFrame) -> List[Tuple[float, float, float]]:
     records = []
     nbr_dist, nbr_ind = neighbors_model.radius_neighbors(current_hits[['x', 'y', 'z']], radius=distances.max(initial=0))
+    nbrn_dist, nbrn_ind = nbr_drop_same_layer(nbr_ind, nbr_dist, current_hits, event)
     for r in distances:
         nbr = [ind[dist <= r] for dist, ind in zip(nbr_dist, nbr_ind)]
-        nbrn = nbr_drop_same_layer(nbr, current_hits, event)
+        nbrn = [ind[dist <= r] for dist, ind in zip(nbrn_dist, nbrn_ind)]
         lnbr = sum(len(arr) - 1 for arr in nbr)  # -1 for the hit being its own neighbor
         lnbrn = sum(len(arr) for arr in nbrn)
         records.append((r, lnbr, lnbrn))
     return records
 
 
+@profile
 def build_segment_neighbor(event, nbr):
     seg_list = []
     for i, ends in enumerate(nbr):
@@ -76,16 +90,19 @@ def build_segment_neighbor(event, nbr):
     return seg
 
 
+@profile
 def stat_seg_neighbors_event_r(neighbors_model: NearestNeighbors, distances: np.ndarray,
                                event: pd.DataFrame) -> pd.Series:
     records = [record for _, g in event.groupby('layer') for record in
                nbr_stat_block(g, neighbors_model, distances, event)]
-    return pd.DataFrame(records, columns=['r', 'all_segments', 'segments_not_same_level']).groupby('r').sum()
+    return pd.DataFrame(records, columns=['r', 'all_segments', 'segments_not_same_level']).groupby(
+        'r').sum().reset_index()
 
 
+@profile
 def stat_seg_neighbors(events: pd.DataFrame) -> pd.DataFrame:
     stat_blocks = []
-    distances = np.linspace(100, 200, 3)
+    distances = np.linspace(300, 3000, 10)
 
     for ei, event in events.groupby(by='event_id'):
         event = event.reset_index(drop=True)
@@ -98,8 +115,8 @@ def stat_seg_neighbors(events: pd.DataFrame) -> pd.DataFrame:
 
 def _profile():
     from datasets import get_hits
-    event = get_hits('trackml_volume', 2)
-    print(stat_seg_neighbors(event.iloc[:10000]))
+    event = get_hits('trackml_volume', 1)
+    print(stat_seg_neighbors(event))
 
 
 if __name__ == '__main__':
