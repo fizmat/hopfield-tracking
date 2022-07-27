3791d99f6054b7a11b1be8ec0cff960f9ebf7219
Ivan Kadochnikov <kadivas@jinr.ru>
2022-07-27 15:57:28 +0300

    refactor(kde): extract function that plots angle-intercept distributions

--- segment/filter_kde.py
+++ segment/filter_kde.py
@@ -1,3 +1,5 @@
+from typing import Tuple
+
 import holoviews as hv
 from matplotlib import pyplot as plt
 import numpy as np
@@ -10,7 +12,7 @@ from segment.track import gen_seg_track_sequential
 from tracking.hit import add_cylindric_coordinates
 
 
-def seg_angle(cyl, seg):
+def seg_angle(cyl: np.ndarray, seg: np.ndarray) -> Tuple[np.ndarray, np.ndarray]:
     starts = cyl[seg[:, 0]]
     ends = cyl[seg[:, 1]]
     r1 = starts[:, 0]
@@ -23,38 +25,23 @@ def seg_angle(cyl, seg):
     return angle, intercept
 
 
-def get_gaussian(events):
+def hits_to_seg_angle_intecept(hits: pd.DataFrame) -> pd.DataFrame:
     all_g = []
-    for ei, ev in events.groupby(by='event_id'):
-        hits = ev.reset_index(drop=True)
-        seg = gen_seg_track_sequential(hits)
-        cyl = hits[['r', 'z', 'phi']].values
+    for _, event in hits.groupby(by='event_id'):
+        event.reset_index(drop=True, inplace=True)
+        seg = gen_seg_track_sequential(event)
+        cyl = event[['r', 'z', 'phi']].values
         angle, intercept = seg_angle(cyl, seg)
-        gaussian = pd.DataFrame({'angle': angle, 'intercept': intercept})
-        all_g.append(gaussian)
-    gaussian = pd.concat(all_g, ignore_index=True)
-    gaussian['directed_angle'] = gaussian.angle
-    gaussian.angle = gaussian.angle.where(gaussian.angle > -np.pi / 2, np.pi + gaussian.angle)
-    gaussian.angle = gaussian.angle.where(gaussian.angle < np.pi / 2, gaussian.angle - np.pi)
-    return gaussian
+        angle_intecept = pd.DataFrame({'angle': angle, 'intercept': intercept})
+        all_g.append(angle_intecept)
+    angle_intecept = pd.concat(all_g, ignore_index=True)
+    angle_intecept['directed_angle'] = angle_intecept.angle
+    angle_intecept.angle = angle_intecept.angle.where(angle_intecept.angle > -np.pi / 2, np.pi + angle_intecept.angle)
+    angle_intecept.angle = angle_intecept.angle.where(angle_intecept.angle < np.pi / 2, angle_intecept.angle - np.pi)
+    return angle_intecept
 
 
-def _profile():
-    min_angle = -2
-    max_angle = 2
-    min_intercept = -20
-    max_intercept = 20
-    size_portion = 1.0
-    nx = 201
-    ny = 201
-
-    bandwidth = 0.2
-    rtol = 0.1
-    atol = 0
-
-    hits = get_hits('trackml', 2)
-    add_cylindric_coordinates(hits)
-    gaussian = get_gaussian(hits)
+def plot_angle_intercept(gaussian: pd.DataFrame, max_intercept: float) -> None:
     gaussian[np.abs(gaussian.intercept) < 1e4].plot.scatter('angle', 'intercept')
     plt.show()
     with pd.option_context('mode.use_inf_as_na', True):
@@ -69,8 +56,8 @@ def _profile():
     plt.xticks(np.logspace(np.log10(1e-10), np.log10(1e14), 25))
     plt.show()
     gaussian[np.abs(gaussian.intercept) < max_intercept].plot.hexbin('directed_angle', 'intercept',
-                                                           bins='log',
-                                                           gridsize=256, figsize=(16, 12), sharex=False)
+                                                                     bins='log',
+                                                                     gridsize=256, figsize=(16, 12), sharex=False)
     plt.show()
     sns.jointplot(
         x="angle",
@@ -80,20 +67,30 @@ def _profile():
         data=gaussian[np.abs(gaussian.intercept) < max_intercept]
     )
     gaussian[np.abs(gaussian.intercept) < max_intercept].plot.hexbin('angle', 'intercept', gridsize=256, bins='log',
-                                                           figsize=(16, 12), sharex=False)
+                                                                     figsize=(16, 12), sharex=False)
     plt.show()
 
+
+def plot(hits: pd.DataFrame, max_intercept=20, min_intercept=None, nx=201, ny=201,
+         bandwidth=0.2, rtol=0.1, atol=0, max_angle=2, min_angle=None) -> None:
+    if min_intercept is None:
+        min_intercept = -max_intercept
+    if min_angle is None:
+        min_angle = -max_angle
+    add_cylindric_coordinates(hits)
+    gaussian = hits_to_seg_angle_intecept(hits)
+    plot_angle_intercept(gaussian, max_intercept)
     portion = gaussian[np.abs(gaussian.intercept) < max_intercept * 10].copy()
     portion.intercept /= 10
-    X = portion.sample(frac=size_portion)[['angle', 'intercept']].to_numpy()
+    X = portion[['angle', 'intercept']].to_numpy()
     kde = KernelDensity(kernel='gaussian', bandwidth=bandwidth, rtol=rtol, atol=atol).fit(X)
-    xx = np.mgrid[min_angle:max_angle:nx * 1j, -max_intercept:max_intercept:ny * 1j].reshape((2, -1)).T
+    xx = np.mgrid[-max_angle:max_angle:nx * 1j, -max_intercept:max_intercept:ny * 1j].reshape((2, -1)).T
     zz = kde.score_samples(xx)
-    plt.imshow(zz.reshape(nx, ny).T, extent=[min_angle, max_angle, -max_intercept, max_intercept],
+    plt.imshow(zz.reshape(nx, ny).T, extent=[-max_angle, max_angle, -max_intercept, max_intercept],
                aspect='auto')
     plt.colorbar()
     plt.show()
-    plt.imshow(zz.reshape(nx, ny).T > -6, extent=[min_angle, max_angle, -max_intercept, max_intercept],
+    plt.imshow(zz.reshape(nx, ny).T > -6, extent=[-max_angle, max_angle, -max_intercept, max_intercept],
                aspect='auto')
     plt.show()
 
@@ -102,7 +99,7 @@ def _profile():
     plt.show()
     zz1 = pd.DataFrame(data=zz.reshape(nx, ny))
     zz1.to_csv(r"zz.csv", index=False, sep=',')
-    size_pixel_x = (max_angle - min_angle) / (nx - 1)
+    size_pixel_x = 2 * max_angle / (nx - 1)
     size_pixel_y = (max_intercept - min_intercept) / (ny - 1)
 
     gaussian['pixel_x'] = ((gaussian.angle - min_angle) // size_pixel_x).astype(int)
@@ -116,6 +113,7 @@ def _profile():
     gaussian.loc[gaussian.pixel_y < 0, 'kde'] = gaussian[condition].kde.min()
     gaussian.loc[gaussian.pixel_y > (ny - 1), 'kde'] = gaussian[condition].kde.min()
     gaussian.kde.hist(bins=50, density=True, cumulative=True)
+    plt.show()
     gaussian.kde.hist(bins=200,
                       log=True, density=True, cumulative=1,
                       figsize=(10, 8)
@@ -125,4 +123,5 @@ def _profile():
 
 
 if __name__ == '__main__':
-    _profile()
+    hits = get_hits('trackml', 10)
+    plot(hits)
