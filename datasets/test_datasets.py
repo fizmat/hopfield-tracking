882430ff1ebc93df38f52123a7527e4da12ab229
Ivan Kadochnikov <kadivas@jinr.ru>
2022-08-08 14:47:09 +0300

    refactor(tests): parametrize get_hits tests to improve fast coverage

--- datasets/test_datasets.py
+++ datasets/test_datasets.py
@@ -3,16 +3,19 @@ import pytest
 from datasets import get_hits
 
 
-def test_get_hits_one_event():
-    for dataset in ('simple', 'bman', 'spdsim', 'trackml', 'trackml_volume'):
-        events = get_hits(dataset, 1)
-        assert len(events.event_id.unique()) == 1
+@pytest.mark.parametrize("dataset", ('simple', 'bman', 'spdsim', 'trackml', 'trackml_volume'))
+def test_get_hits_one_event(dataset):
+    events = get_hits(dataset, 1)
+    assert len(events.event_id.unique()) == 1
 
 
-@pytest.mark.trackml
-@pytest.mark.bman
-@pytest.mark.slow
-def test_get_hits():
-    for dataset in ('simple', 'bman', 'spdsim', 'trackml', 'trackml_volume'):
-        events = get_hits(dataset, 2)
-        assert len(events.event_id.unique()) == 2
+@pytest.mark.parametrize("dataset",
+                         ('simple', 'spdsim',
+                          pytest.param('bman', marks=(pytest.mark.bman, pytest.mark.slow)),
+                          pytest.param('trackml', marks=pytest.mark.trackml),
+                          pytest.param('trackml_volume', marks=pytest.mark.trackml)
+                          )
+                         )
+def test_get_hits_two_events(dataset):
+    events = get_hits(dataset, 2)
+    assert len(events.event_id.unique()) == 2
