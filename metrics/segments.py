12646687145d17052245cd128213b3991b598237
Ivan Kadochnikov <kadivas@jinr.ru>
2022-08-09 16:28:49 +0300

    perf(metrics): even faster perfect_act with pandas index join

--- metrics/segments.py
+++ metrics/segments.py
@@ -5,10 +5,12 @@ import pandas as pd
 def gen_perfect_act(seg: np.ndarray, tseg: np.ndarray) -> np.ndarray:
     # Can't just use hits.track[a] = hits.track[b]: if there's a segment connecting far away hits in the track,
     # they should not activate.
-    dfseg = pd.DataFrame(seg, columns=('a', 'b'))
-    dftseg = pd.DataFrame(tseg, columns=('a', 'b'))
-    df = pd.merge(dfseg, dftseg, on=['a', 'b'], how='left', indicator=True)
-    return (df._merge == 'both').to_numpy().astype(float)
+    perfect_act = np.zeros(len(seg))
+    dfseg = pd.MultiIndex.from_arrays([seg[:, 0], seg[:, 1]])
+    dftseg = pd.MultiIndex.from_arrays([tseg[:, 0], tseg[:, 1]])
+    _, indexer, __ = dfseg.join(dftseg, how='inner', return_indexers=True)
+    perfect_act[indexer] = 1.
+    return perfect_act
 
 
 def _profile():
