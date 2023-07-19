bcb160832c825f7884c1513f9e0240d37e26e455
Ivan Kadochnikov <kadivas@jinr.ru>
2023-07-19 10:58:05 +0400

    feat(optimizer): replace hpbandster with smac and test

--- docker/test_smac.py
+++ docker/test_smac.py
@@ -2,18 +2,21 @@ from ConfigSpace import ConfigurationSpace
 from smac.facade.smac_bb_facade import SMAC4BB
 from smac.scenario.scenario import Scenario
 
-def evaluate(config):
-    return config['x']**2 + 10*config['y']**2
 
-def main():
+def evaluate(config):
+    return config['x'] ** 2 + 10 * config['y'] ** 2
+
+
+def test_smac():
     scenario = Scenario({
-            'run_obj': 'quality',
-            'runcount-limit': 10,
-            'cs': ConfigurationSpace({'x': (-1., 1.), 'y': (-1., 1.)})
-        })
+        'run_obj': 'quality',
+        'runcount-limit': 10,
+        'cs': ConfigurationSpace({'x': (-1., 1.), 'y': (-1., 1.)})
+    })
     optimizer = SMAC4BB(scenario=scenario, tae_runner=evaluate)
     best_config = optimizer.optimize()
     print(best_config)
 
+
 if __name__ == '__main__':
-    main()
+    test_smac()
