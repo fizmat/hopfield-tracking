96ae94bd40eb90d6a27f5d285560cc9b267cad59
Ivan Kadochnikov <kadivas@jinr.ru>
2023-08-02 16:31:03 +0400

    profile(tseg): reorder grouping in seg_track_layered for performance

--- segment/track.py
+++ segment/track.py
@@ -10,10 +10,22 @@ def gen_seg_track_sequential(event: pd.DataFrame) -> np.ndarray:
 
 def gen_seg_track_layered(event: pd.DataFrame) -> np.ndarray:
     track_segments = []
-    for track, g in event[event.track >= 0].groupby('track'):
-        layers = g.groupby('layer').groups
-        for layer, starts in layers.items():
-            for b in layers.get(layer + 1, ()):
-                for a in starts:
-                    track_segments.append((a, b))
+    event = event.loc[event.track >= 0]
+    d = {layer: g.groupby('track').groups
+         for layer, g in event.groupby('layer')}
+    for layer, g in d.items():
+        for track, start_hits in g.items():
+            for b in d.get(layer + 1, {}).get(track, ()):
+                track_segments += [(a, b) for a in start_hits]
     return np.array(track_segments)
+
+
+def main():
+    from datasets.trackml import get_hits_trackml_one_event
+    event = get_hits_trackml_one_event()
+    gen_seg_track_layered(event)
+    gen_seg_track_sequential(event)
+
+
+if __name__ == '__main__':
+    main()
